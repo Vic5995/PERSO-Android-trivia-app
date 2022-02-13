0b57ad37b8e45d54acee2ae6ca744097571383b3
Victoire <victoire.lenglart.5995@gmail.com>
2022-02-13 14:43:55 +0100

    ✨ App Controller settings for Volley

--- app/src/main/java/com/example/triviaapp/MainActivity.java
+++ app/src/main/java/com/example/triviaapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    String URL = "https://opentdb.com/api.php?amount=30&category=9&type=boolean";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
